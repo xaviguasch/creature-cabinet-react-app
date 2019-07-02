2a7ad8298332762ef8263a7e994832cff12c7407
xaviguasch <xaviguasch@gmail.com>
2019-07-02 21:23:58 +0200

    fetched the creatures from an API

--- src/App.js
+++ src/App.js
@@ -6,21 +6,14 @@ class App extends Component {
     super()
 
     this.state = {
-      monsters: [
-        {
-          name: 'Frankenstein',
-          id: 'asc1'
-        },
-        {
-          name: 'Dracula',
-          id: 'asc2'
-        },
-        {
-          name: 'Zombie',
-          id: 'asc3'
-        }
-      ]
+      monsters: []
     }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }))
   }
 
   render() {
