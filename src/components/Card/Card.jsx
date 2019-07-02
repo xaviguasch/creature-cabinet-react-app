c9d79b1bea0f07b6f66d214f78d00464f7853fde
xaviguasch <xaviguasch@gmail.com>
2019-07-02 22:50:01 +0200

    added Card component

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Card.css'
+
+const Card = props => {
+  return (
+    <div className='card-container'>
+      <img
+        src={`https://robohash.org/${props.monster.id}?set=set2&180x180`}
+        alt='monster'
+      />
+      <h2>{props.monster.name}</h2>
+      <p>{props.monster.email}</p>
+    </div>
+  )
+}
+
+export default Card
--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Card from '../Card/Card'
 import './CardList.css'
 
 const CardList = props => {
-  return <div className='card-list'>{props.children}</div>
+  return (
+    <div className='card-list'>
+      {props.monsters.map(monster => (
+        <Card key={monster.id} monster={monster} />
+      ))}
+    </div>
+  )
 }
 
 export default CardList
