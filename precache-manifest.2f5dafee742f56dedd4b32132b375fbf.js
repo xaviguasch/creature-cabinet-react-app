c5c9d5938d3d9256b9fca529fc43ab1e9eb312ec
xaviguasch <xaviguasch@gmail.com>
2019-07-08 21:18:42 +0200

    Updates

---
+++ precache-manifest.2f5dafee742f56dedd4b32132b375fbf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dbfe95d45e58d63359e837d8371b21d",
+    "url": "/creature-cabinet-react-app/index.html"
+  },
+  {
+    "revision": "210afa8ec2daec479169",
+    "url": "/creature-cabinet-react-app/static/css/main.f7dfc3ba.chunk.css"
+  },
+  {
+    "revision": "d82afacfb8046780bdff",
+    "url": "/creature-cabinet-react-app/static/js/2.c272f57a.chunk.js"
+  },
+  {
+    "revision": "210afa8ec2daec479169",
+    "url": "/creature-cabinet-react-app/static/js/main.7b7157d3.chunk.js"
+  },
+  {
+    "revision": "04285f8b33aa6283f628",
+    "url": "/creature-cabinet-react-app/static/js/runtime~main.18f557a5.js"
+  }
+]);
